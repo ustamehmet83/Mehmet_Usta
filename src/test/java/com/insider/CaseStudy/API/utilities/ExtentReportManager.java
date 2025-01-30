04b00ff64b76a172c784bb3980d287f38b83de2b
Mehmet Usta <ustamehmet83@hotmail.com>
2025-01-30 20:35:16 +0300

    second commit

--- src/test/java/com/insider/CaseStudy/API/utilities/ExtentReportManager.java
+++ src/test/java/com/insider/CaseStudy/API/utilities/ExtentReportManager.java
@@ -4,6 +4,7 @@ import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.Status;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.aventstack.extentreports.reporter.ExtentKlovReporter;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.aventstack.extentreports.reporter.configuration.Theme;
 import org.junit.jupiter.api.extension.*;
@@ -13,54 +14,58 @@ import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class ExtentReportManager implements BeforeAllCallback,AfterEachCallback, AfterAllCallback, TestExecutionExceptionHandler  {
+public class ExtentReportManager implements BeforeAllCallback, BeforeEachCallback, AfterEachCallback, AfterAllCallback, TestExecutionExceptionHandler {
 
-	public ExtentHtmlReporter extentHtmlReporter;
-	public ExtentReports extentReports;
-	public ExtentTest extentTest;
-	public String repName;
+    public ExtentHtmlReporter extentHtmlReporter;
+    public ExtentReports extentReports;
+    public ExtentTest extentTest;
+    public ExtentTest node;
+    public String repName;
 
-	@Override
-	public void beforeAll(ExtensionContext context) {
+    @Override
+    public void beforeAll(ExtensionContext context) {
 
-		String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
-		repName = "Test-Report-" + timeStamp + ".html";
-		String reportPath = System.getProperty("user.dir") + "\\reports\\" + repName;
+        String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
+        repName = "Test-Report-" + timeStamp + ".html";
+        String reportPath = System.getProperty("user.dir") + "\\reports\\" + repName;
 
-		extentReports = new ExtentReports();
-		extentHtmlReporter = new ExtentHtmlReporter (reportPath);
-		extentHtmlReporter.config().setDocumentTitle("InsiderCaseStudy");
-		extentHtmlReporter.config().setReportName("Pet Store Users API");
-		extentHtmlReporter.config().setTheme(Theme.DARK);
-		extentReports.attachReporter(extentHtmlReporter);
-		extentHtmlReporter.config().setDocumentTitle("Extent Report");
-		extentTest=extentReports.createTest("ExtentTest","Test Report");
-	}
+        extentReports = new ExtentReports();
+        extentHtmlReporter = new ExtentHtmlReporter(reportPath);
+        extentHtmlReporter.config().setDocumentTitle("InsiderCaseStudy");
+        extentHtmlReporter.config().setReportName("Pet Store Users API");
+        extentHtmlReporter.config().setTheme(Theme.DARK);
 
-	@Override
-	public void afterEach(ExtensionContext context) {
+        extentReports.attachReporter(extentHtmlReporter);
+        extentHtmlReporter.config().setDocumentTitle("Extent Report");
+        extentTest = extentReports.createTest("ExtentTest", "Test Report");
 
-        if (extentTest.getStatus()!=Status.FAIL){
-			extentTest=extentReports.createTest(context.getDisplayName());
-            extentTest.createNode(context.getDisplayName());
-            extentTest.log(Status.PASS, "Test Passed");
+    }
+
+    @Override
+    public void beforeEach(ExtensionContext context) {
+        node = extentTest.createNode(context.getDisplayName());
+    }
+
+    @Override
+    public void afterEach(ExtensionContext context) {
+        if (node.getStatus() != Status.FAIL) {
+            node.log(Status.PASS, "Test Passed");
         }
-	}
+    }
 
 
-	@Override
-	public void afterAll(ExtensionContext context) {
-		extentReports.flush();
-	}
+    @Override
+    public void afterAll(ExtensionContext context) {
+        extentReports.flush();
+    }
 
-	@Override
-	public void handleTestExecutionException(ExtensionContext context, Throwable throwable) throws Throwable {
-            extentTest = extentReports.createTest(context.getDisplayName());
-            extentTest.log(Status.FAIL, "Test Execution Exception");
-            extentTest.log(Status.FAIL, throwable.getMessage());
-            throw throwable;
+    @Override
+    public void handleTestExecutionException(ExtensionContext context, Throwable throwable) throws Throwable {
+        node.log(Status.FAIL, "Test Execution Exception");
+        node.log(Status.FAIL, throwable.getMessage());
+        throw throwable;
 
-	}
+    }
 
 
 }
